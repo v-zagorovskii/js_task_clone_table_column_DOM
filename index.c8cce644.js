39fc82e0e3d5c65c53858f497ff05184caf8a0bf
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-17 17:01:06 +0300

    make build

---
+++ index.c8cce644.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var r=e.children,l=r[1].cloneNode(!0);e.insertBefore(l,r[r.length-1])});
+//# sourceMappingURL=index.c8cce644.js.map
